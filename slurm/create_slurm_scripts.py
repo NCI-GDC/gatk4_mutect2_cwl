f02026781b6d4f37c0f5c6fb2d2d77e60e5d9f32
Shenglai Li <sli6@uchicago.edu>
2018-04-26 13:43:46 -0500

    feat(tumor_only): add tumor only pipe

--- slurm/create_slurm_scripts.py
+++ slurm/create_slurm_scripts.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     required.add_argument('--java_heap', help='Java heap mem', required=True)
     required.add_argument("--postgres_config", help="Path to postgres config file", required=True)
     required.add_argument("--outdir", default="./", help="Output directory for slurm scripts")
-    required.add_argument("--pipeline", choices=['vc', 'pon'], help="Calling mutect2 on tumor normal pair or creating pon on normal", required=True)
+    required.add_argument("--pipeline", choices=['tumor_only', 'pon'], help="Calling mutect2 on tumor only calling or creating pon on normal", required=True)
     required.add_argument("--input_table", help="Postgres input table name", required=True)
     required.add_argument("--input_primary_id", default="id", help="Primary id for postgres input table")
     required.add_argument("--status_table", default="None", help="Postgres status table name")
@@ -89,10 +89,67 @@ if __name__ == "__main__":
                     line = line.replace("XX_BLOCKSIZE_XX", str(args.block))
                 if "XX_JAVAHEAP_XX" in line:
                     line = line.replace("XX_JAVAHEAP_XX", args.java_heap)
+                if "XX_CWL_XX" in line:
+                    line = line.replace("XX_CWL_XX", "tools/gatk4_mutect2.cwl")
                 slurm.write(line)
             slurm.close()
             temp.close()
-    elif args.pipeline == 'vc':
-        sys.exit('Pipeline Not ready')
+    elif args.pipeline == 'tumor_only':
+        engine = postgres.utils.get_db_engine(args.postgres_config)
+        cases = postgres.status.get_mutect2_case(engine, str(args.input_table), str(args.status_table), input_primary_column=str(args.input_primary_id))
+        for case in cases:
+            tumor_s3 = utils.s3.check_s3url(cases[case][3])
+            slurm = open(os.path.join(args.outdir, "%s.%s.sh" %(args.pipeline, cases[case][1])), "w")
+            template = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+            "etc/template.sh")
+            temp = open(template, "r")
+            for line in temp:
+                if "XX_THREAD_COUNT_XX" in line:
+                    line = line.replace("XX_THREAD_COUNT_XX", str(args.thread_count))
+                if "XX_MEM_XX" in line:
+                    line = line.replace("XX_MEM_XX", str(args.mem))
+                if "XX_CASEID_XX" in line:
+                    line = line.replace("XX_CASEID_XX", str(cases[case][0]))
+                if "XX_TID_XX" in line:
+                    line = line.replace("XX_TID_XX", str(cases[case][1]))
+                if "XX_NID_XX" in line:
+                    line = line.replace("XX_NID_XX", str(cases[case][2]))
+                if "XX_FILESIZE_XX" in line:
+                    line = line.replace("XX_FILESIZE_XX", "None")
+                if "XX_TS3URL_XX" in line:
+                    line = line.replace("XX_TS3URL_XX", tumor_s3['s3url'])
+                if "XX_TUMOR_BARCODE_XX" in line:
+                    line = line.replace("XX_TUMOR_BARCODE_XX", str(cases[case][5]))
+                if "XX_TS3PROFILE_XX" in line:
+                    line = line.replace("XX_TS3PROFILE_XX", tumor_s3['profile'])
+                if "XX_TS3ENDPOINT_XX" in line:
+                    line = line.replace("XX_TS3ENDPOINT_XX", tumor_s3['endpoint'])
+                if "XX_NS3URL_XX" in line:
+                    line = line.replace("XX_NS3URL_XX", "None")
+                if "XX_NORMAL_BARCODE_XX" in line:
+                    line = line.replace("XX_NORMAL_BARCODE_XX", "None")
+                if "XX_NS3PROFILE_XX" in line:
+                    line = line.replace("XX_NS3PROFILE_XX", "None")
+                if "XX_NS3ENDPOINT_XX" in line:
+                    line = line.replace("XX_NS3ENDPOINT_XX", "None")
+                if "XX_PIPELINE_XX" in line:
+                    line = line.replace("XX_PIPELINE_XX", args.pipeline)
+                if "XX_REFDIR_XX" in line:
+                    line = line.replace("XX_REFDIR_XX", args.refdir)
+                if "XX_S3DIR_XX" in line:
+                    line = line.replace("XX_S3DIR_XX", upload_s3['s3url'])
+                if "XX_S3PROFILE_XX" in line:
+                    line = line.replace("XX_S3PROFILE_XX", upload_s3['profile'])
+                if "XX_S3ENDPOINT_XX" in line:
+                    line = line.replace("XX_S3ENDPOINT_XX", upload_s3['endpoint'])
+                if "XX_BLOCKSIZE_XX" in line:
+                    line = line.replace("XX_BLOCKSIZE_XX", str(args.block))
+                if "XX_JAVAHEAP_XX" in line:
+                    line = line.replace("XX_JAVAHEAP_XX", args.java_heap)
+                if "XX_CWL_XX" in line:
+                    line = line.replace("XX_CWL_XX", "workflows/gatk4_mutect2_single_tumor_calling.cwl")
+                slurm.write(line)
+            slurm.close()
+            temp.close()
     else:
         raise Exception("Cannot get correct pipeline: %s. Please choose between 'mutect2' or 'pon'." % args.pipeline)
